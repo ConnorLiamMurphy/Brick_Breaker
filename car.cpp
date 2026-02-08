d33831d0d2b353b7dc94f9da1663a228d77a6c72
ConnorLiamMurphy <con.el9125@gmail.com>
2026-02-08 12:54:32 -0500

    init setup 2

---
+++ car.cpp
@@ -0,0 +1,52 @@
+#include "car.hpp"
+#include "engine.hpp"
+#include "game_objects.hpp"
+#include <SDL3/SDL.h>
+
+// Constructor
+Car::Car(){
+	// Access the sprite component to get our rectangle
+	// (destination rectangle)
+	auto* spriteComponent = addComponent<SpriteComponent>();
+	spriteComponent->loadSprite(Engine::instance().getRenderer(), "sprite.png");
+	rect = spriteComponent->getRect();
+
+}
+
+// Called once per frame
+void Car::update(float deltaTime) {
+	// Call the parent class to notify components
+	GameObject::update(deltaTime);
+	for(auto it = Engine::keyEvents.begin(); it != Engine::keyEvents.end(); ++it){
+		if(it->key.key == SDLK_W){
+			up(deltaTime);
+		}
+		if(it->key.key == SDLK_S){
+			down(deltaTime);
+		}
+		if(it->key.key == SDLK_A){
+			left(deltaTime);
+		}
+		if(it->key.key == SDLK_D){
+			right(deltaTime);
+		}
+	}
+}
+
+// Movement should be based on the time that has passed
+// for smoothest motion.
+void Car::up(float dt){
+	rect->y -= pps * dt;
+}
+
+void Car::down(float dt){
+	rect->y += pps * dt;
+}
+
+void Car::left(float dt){
+	rect->x -= pps * dt;
+}
+
+void Car::right(float dt){
+	rect->x += pps * dt;
+}
---
+++ hello.cpp
@@ -0,0 +1,16 @@
+#include "car.hpp"
+#include "engine.hpp"
+#include "game_objects.hpp"
+
+int main(int argc, char** argv) {
+    // Create the objects that make up our game, and start the run.
+    Engine& engine = Engine::instance();
+
+    Scene scene;
+    Car car;
+
+    scene.addObject(&car);
+    engine.setScene(&scene);
+    engine.run();
+}
+
